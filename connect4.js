7f15c50709e417a7252e9c8f90c3691daaef4e41
Mohamed Abdelrazek <abdelrazekm543@gmail.com>
2020-11-18 13:50:58 +0000

    add hover over column functionality

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4{
         this.COLUMNS=7;
         this.selector = selector; 
         this.createGrid();
+        this.setupEventListeners();
     }
     createGrid() {
         const $board = $(this.selector);
@@ -12,10 +13,41 @@ class Connect4{
             .addClass('row');
             for(var col=0;col<this.COLUMNS;col++) {
                 const $col=$('<div>')
-                .addClass('col empty');
+                .addClass('col empty')
+                .attr('data-col',col)
+                .attr('data-row',row);
                 $row.append($col);
             }
             $board.append($row);
         }
     }
+    
+
+    setupEventListeners(){
+        console.log("DDDDDDDDDD");
+        const $board = $(this.selector);
+
+        function findLastEmptyCell(col){
+            const cells = $(`.col[data-col='${col}']`)
+            for (let i =(cells.length-1);i>=0; i--) {
+                const $cell = $(cells[i]);
+                if($cell.hasClass('empty')){
+                    return $cell;
+                }
+            }
+            return null;
+        }
+
+        $board.on('mouseenter','.col.empty',function(){
+            const col = $(this).data('col');
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.addClass(`next-red`);
+        });
+        $board.on('mouseleave','.col.empty',function(){
+            $('.col').removeClass('next-red')
+        });
+    }
+
+
+
 }
